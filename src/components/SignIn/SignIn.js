5bade43a38e5b4098b712e221db8d6e7196c420a
Elder Patten Ferreira <elderferreirass@gmail.com>
2019-08-20 22:38:09 -0600

    cleaned project

--- src/components/SignIn/SignIn.js
+++
@@ -1,7 +0,0 @@
-import {withAuthenticator} from "aws-amplify-react";
-
-const signin = () => {
-    return null;
-};
-
-export default withAuthenticator(signin);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,12 +4,7 @@ import Home from "../../components/Home/Home";
 import Contact from "../../components/Contact/Contact";
 import About from "../../components/About/About";
 import Search from "../../containers/Search/Search";
-import SignIn from "../../components/SignIn/SignIn";
 import './App.scss';
-
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from '../../aws-exports';
-Amplify.configure(awsconfig);
 
 class App extends Component {
     render() {
@@ -19,7 +14,6 @@ class App extends Component {
                 <Route path="/about" component={About} exact/>
                 <Route path="/contact" component={Contact} exact/>
                 <Route path="/search" component={Search} exact/>
-                <Route path="/sign-in" component={SignIn} exact/>
             </div>
         );
     }
