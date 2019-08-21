0b5897d539dbebfac5114b936c25703688271abd
Elder Patten Ferreira <elderferreirass@gmail.com>
2019-08-20 21:39:13 -0600

    added sign in and sign up

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,7 @@
+import {withAuthenticator} from "aws-amplify-react";
+
+const signin = () => {
+    return null;
+};
+
+export default withAuthenticator(signin);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import Home from "../../components/Home/Home";
 import Contact from "../../components/Contact/Contact";
 import About from "../../components/About/About";
 import Search from "../../containers/Search/Search";
-
+import SignIn from "../../components/SignIn/SignIn";
 import './App.scss';
 
-function App() {
-    return (
-        <div className="App">
-            <Route path="/" component={Home} exact/>
-            <Route path="/about" component={About} exact/>
-            <Route path="/contact" component={Contact} exact/>
-            <Route path="/search" component={Search} exact/>
-        </div>
-    );
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../../aws-exports';
+Amplify.configure(awsconfig);
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Route path="/" component={Home} exact/>
+                <Route path="/about" component={About} exact/>
+                <Route path="/contact" component={Contact} exact/>
+                <Route path="/search" component={Search} exact/>
+                <Route path="/sign-in" component={SignIn} exact/>
+            </div>
+        );
+    }
 }
 
 export default App;
