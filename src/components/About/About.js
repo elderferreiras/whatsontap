9591c2800bc7dadc4cc76e4e680eae0bab6a6477
Elder Patten Ferreira <elderferreirass@gmail.com>
2019-08-28 20:28:08 -0600

    updated contact text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,14 +9,20 @@ const about = () => {
                     <div className="content-section-heading">
                         <h2 className="mb-5">About</h2>
                     </div>
-                    <div className="row">
+                    <div className="row" style={{backgroundColor: '#0000006b', paddingTop: '20px', paddingBottom: '20px'}}>
                         <div className="col align-self-center">
-                            <p className="text-faded mb-5">
+                            <p className="text-faded mb-5 text-left">
                                 If you ever wondered what's on tap before going to a brewery, you're in the right
                                 place. <em>What's on tap?</em> is a list of all American breweries and their beers.
                             </p>
-                            <p className="text-faded mb-0">
-                                If by any chance you cannot find your favorite brewery in here, come back later. We're currently working on it.
+                            <p className="text-faded mb-5  text-left">
+                                If by any chance you cannot find your favorite brewery in here, come back later. We're currently working on it. If you would like to help getting the beers list updated, let us know.
+                            </p>
+                            <p className="text-faded mb-5  text-left">
+                                Also, stay tuned for the mobile app.
+                            </p>
+                            <p className="text-faded mb-0 text-right">
+                              Elder Patten Ferreira
                             </p>
                         </div>
                     </div>
